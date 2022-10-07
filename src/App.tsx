cf796d3c5404227484c26b486205267d3452e2ce
Nathan Pope <nathan.pope@bbc.co.uk>
2022-10-07 17:29:29 +0100

    WIP Experimenting with ways to render wordle grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import React from "react";
 import styled from "styled-components";
-// import WordleGrid from "./components/wordle-grid";
+import WordleGrid from "./components/wordle-grid";
 
 const StyledApp = styled.div`
-  text-align: center;
+  display: block;
+`;
+
+const StyledHeader = styled.header`
+  display: flex;
+  background-color: #2d468c;
+  padding: 1rem;
 `;
 
 const StyledTitle = styled.h1`
-  margin: 0 auto;
-  padding: 1rem;
-  background-color: #2d468c;
   color: #eeeeee;
+  margin: 0 auto;
 `;
 
 const App = () => {
   return (
     <StyledApp>
-      <StyledTitle>It's Another Wordle Clone! 🤪</StyledTitle>
+      <StyledHeader>
+        <StyledTitle>It's Another Wordle Clone! 🤪</StyledTitle>
+      </StyledHeader>
+      <WordleGrid />
     </StyledApp>
   );
 };
--- src/components/wordle-grid.tsx
+++ src/components/wordle-grid.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledGrid = styled.div`
-  width: auto;
+const StyledGridContainer = styled.div`
+  display: flex;
 `;
+
+const StyledGrid = styled.div`
+  display: grid;
+`;
+
+interface IProps {
+  children: string;
+}
+
+const Tile = ({ children }: IProps) => {
+  return <div>{children}</div>;
+};
+
+const Row = (): React.ReactElement[] => {
+  const rowStateArray = ["", "", "", "", ""];
+  return rowStateArray.map((tileContent, i) => (
+    <Tile key={i}>{tileContent}</Tile>
+  ));
+};
 
 const WordleGrid = () => {
   return (
-    <StyledGrid>
-      <p>Render test</p>
-    </StyledGrid>
+    <StyledGridContainer>
+      <StyledGrid>
+        <p>Wordle grid</p>
+        {/* <Row /> */}
+      </StyledGrid>
+    </StyledGridContainer>
   );
 };
 
