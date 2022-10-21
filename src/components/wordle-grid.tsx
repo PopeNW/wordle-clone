de42976092a2d61e6fa5c518082609b81179e6d5
Nathan Pope <nathan.pope@bbc.co.uk>
2022-10-21 16:59:20 +0100

    Added types for RowState and GridState. Styled grid cells and centered grid. Changed grid state to contain all state of all rows and mapped it to Row components.

--- src/components/wordle-grid.tsx
+++ src/components/wordle-grid.tsx
@@ -1,8 +1,21 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
+type RowState = [string, string, string, string, string];
+
+type GridState = [RowState, RowState, RowState, RowState, RowState, RowState];
+
+interface ICell {
+  children: string;
+}
+
+interface IRow {
+  rowState: RowState;
+}
+
 const StyledGridContainer = styled.div`
   display: flex;
+  justify-content: center;
 `;
 
 const StyledGrid = styled.div`
@@ -10,39 +23,54 @@ const StyledGrid = styled.div`
 `;
 
 const StyledRow = styled.div`
-  grid: row;
+  display: flex;
+  justify-content: center;
 `;
 
-interface ITile {
-  children: string;
-}
+const StyledCell = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 3.5rem;
+  height: 3.5rem;
+  margin: 0.2rem;
+  border: 0.1rem solid grey;
+  font-size: 2rem;
+  font-weight: bold;
+`;
 
-interface IRow {
-  rowState: string[];
-}
-
-const Tile = ({ children }: ITile) => {
-  return <div>{children}</div>;
+const Cell = ({ children }: ICell) => {
+  return <StyledCell>{children}</StyledCell>;
 };
 
 const Row = ({ rowState }: IRow) => {
   return (
     <StyledRow>
-      {rowState.map((tileContent, i) => (
-        <Tile key={i}>{tileContent}</Tile>
+      {rowState.map((cell, index) => (
+        <Cell key={index}>{cell}</Cell>
       ))}
     </StyledRow>
   );
 };
 
 const WordleGrid = () => {
-  const [row, setRow] = useState(["1", "2", "3", "4", "5"]);
+  // Use state for each cell to avoid re-rendering entire grid
+  const [grid, setGrid] = useState<GridState>([
+    ["G", "O", "O", "S", "E"],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+  ]);
 
   return (
     <StyledGridContainer>
       <StyledGrid>
-        <p>Wordle grid</p>
-        <Row rowState={row} />
+        <p>Wordle Grid</p>
+        {grid.map((row, index) => (
+          <Row key={index} rowState={row} />
+        ))}
       </StyledGrid>
     </StyledGridContainer>
   );
