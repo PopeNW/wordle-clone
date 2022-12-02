f2dcaf4242174d3e6493a571e291e5fdefddfed1
Nathan Pope <nathan.pope@bbc.co.uk>
2022-12-02 16:00:45 +0000

    Expanded app alphabetical character render test - Tests rendering different characters in first row tiles

--- src/tests/app.test.tsx
+++ src/tests/app.test.tsx
@@ -8,16 +8,40 @@ test("should render app title", () => {
   expect(titleElement).toBeInTheDocument();
 });
 
-test("should render alphabetical character in first tile on board", () => {
+test("should render alphabetical characters in first row on board", () => {
   render(<App />);
-  const tileElement = screen.getByTestId("board-row-0-tile-0");
-  const buttonElement = screen.getByText("Q", { selector: "button" });
+  // First row tiles of board
+  const tileElement1 = screen.getByTestId("board-row-0-tile-0");
+  const tileElement2 = screen.getByTestId("board-row-0-tile-1");
+  const tileElement3 = screen.getByTestId("board-row-0-tile-2");
+  const tileElement4 = screen.getByTestId("board-row-0-tile-3");
+  const tileElement5 = screen.getByTestId("board-row-0-tile-4");
 
-  click(buttonElement);
+  // Keyboard button elements
+  const buttonElementQ = screen.getByText("Q", { selector: "button" });
+  const buttonElementW = screen.getByText("W", { selector: "button" });
+  const buttonElementE = screen.getByText("E", { selector: "button" });
+  const buttonElementR = screen.getByText("R", { selector: "button" });
+  const buttonElementT = screen.getByText("T", { selector: "button" });
 
-  expect(tileElement).toHaveTextContent("Q");
+  // Fill first row with alphabetical characters
+  click(buttonElementQ);
+  click(buttonElementW);
+  click(buttonElementE);
+  click(buttonElementR);
+  click(buttonElementT);
+
+  // Assert that board has rendered characters in first row tiles
+  expect(tileElement1).toHaveTextContent("Q");
+  expect(tileElement2).toHaveTextContent("W");
+  expect(tileElement3).toHaveTextContent("E");
+  expect(tileElement4).toHaveTextContent("R");
+  expect(tileElement5).toHaveTextContent("T");
 });
 
-test("should handle click for ENTER key", () => {});
+test("should handle click for ENTER key", () => {
+  render(<App />);
+
+});
 
 test("should handle click for BACKSPACE key", () => {});
