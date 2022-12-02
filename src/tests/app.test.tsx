31ccf9d29d80346d90ddf51ed21dcbc4ad3c0192
Nathan Pope <nathan.pope@bbc.co.uk>
2022-12-02 16:11:12 +0000

    Refactored app render alphabetical character test - Reduced duplication by using forEach loop over a keyboard characters array

--- src/tests/app.test.tsx
+++ src/tests/app.test.tsx
@@ -10,38 +10,21 @@ test("should render app title", () => {
 
 test("should render alphabetical characters in first row on board", () => {
   render(<App />);
-  // First row tiles of board
-  const tileElement1 = screen.getByTestId("board-row-0-tile-0");
-  const tileElement2 = screen.getByTestId("board-row-0-tile-1");
-  const tileElement3 = screen.getByTestId("board-row-0-tile-2");
-  const tileElement4 = screen.getByTestId("board-row-0-tile-3");
-  const tileElement5 = screen.getByTestId("board-row-0-tile-4");
 
-  // Keyboard button elements
-  const buttonElementQ = screen.getByText("Q", { selector: "button" });
-  const buttonElementW = screen.getByText("W", { selector: "button" });
-  const buttonElementE = screen.getByText("E", { selector: "button" });
-  const buttonElementR = screen.getByText("R", { selector: "button" });
-  const buttonElementT = screen.getByText("T", { selector: "button" });
+  const keyboardCharacters = ["Q", "W", "E", "R", "T"];
+  
+  keyboardCharacters.forEach((keyChar, index) => {
+    const tileElement = screen.getByTestId(`board-row-0-tile-${index}`);
+    const buttonElement = screen.getByText(keyChar, { selector: "button" });
 
-  // Fill first row with alphabetical characters
-  click(buttonElementQ);
-  click(buttonElementW);
-  click(buttonElementE);
-  click(buttonElementR);
-  click(buttonElementT);
+    click(buttonElement);
 
-  // Assert that board has rendered characters in first row tiles
-  expect(tileElement1).toHaveTextContent("Q");
-  expect(tileElement2).toHaveTextContent("W");
-  expect(tileElement3).toHaveTextContent("E");
-  expect(tileElement4).toHaveTextContent("R");
-  expect(tileElement5).toHaveTextContent("T");
+    expect(tileElement).toHaveTextContent(keyChar);
+  })
 });
 
 test("should handle click for ENTER key", () => {
   render(<App />);
-
 });
 
 test("should handle click for BACKSPACE key", () => {});
