d25475047231273771197656a77778c2bf7ef886
Nathan Pope <nathan.pope@bbc.co.uk>
2023-06-09 13:48:01 +0100

    Added more detail to game-over tests

--- src/tests/win-lose/game-over.test.tsx
+++ src/tests/win-lose/game-over.test.tsx
@@ -1,11 +1,23 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { selectCharacters, selectEnter } from "../helpers/keyboard-input";
 import App from "../../app";
 
-test("should handle a game win when player correctly guesses the word", () => {
-  // TODO
+test("game ends with win when guessing correct word", () => {
+  render(<App wordle="DUCKS" />);
+
+  selectCharacters(["D", "U", "C", "K", "S"]);
+  selectEnter();
+
+  // Expect game over modal with win
 });
 
 test("should handle a game loss when player runs out of guesses", () => {
-  // TODO
+  render(<App wordle="GOOSE" />);
+
+  for (let i = 0; i < 6; i++) {
+    selectCharacters(["D", "U", "C", "K", "S"]);
+    selectEnter();
+  }
+
+  // Expect game over modal with loss
 });
